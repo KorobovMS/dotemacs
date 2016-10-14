942422ae5e2cb2d8ab011424d9dc2e026dc25e32
Korobov Mikhail <korobovms94@gmail.com>
2016-10-14 22:04:37 +0300

    Base config was added

---
+++ init.el
@@ -0,0 +1,12 @@
+;; Mouse speed
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-scroll-amount '(2))
+
+;; UI
+(tool-bar-mode -1)
+(setq default-cursor-type 'bar)
+
+(setq-default tab-width 4)
+
+(custom-set-faces
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 120 :width normal)))))
