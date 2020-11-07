fa7464c51af5a3a6cc1bf8489c5da57549d69301
Mikhail Korobov <korobovms94@gmail.com>
2020-11-07 17:53:27 +0300

    Now shortcuts can be performed in Russian layout

--- init.el
+++ init.el
@@ -10,3 +10,30 @@
 
 (custom-set-faces
  '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 120 :width normal)))))
+
+
+(defun reverse-input-method (input-method)
+  "Build the reverse mapping of single letters from INPUT-METHOD."
+  (interactive
+   (list (read-input-method-name "Use input method (default current): ")))
+  (if (and input-method (symbolp input-method))
+      (setq input-method (symbol-name input-method)))
+  (let ((current current-input-method)
+	(modifiers '(nil (control) (meta) (control meta))))
+    (when input-method
+      (activate-input-method input-method))
+    (when (and current-input-method quail-keyboard-layout)
+      (dolist (map (cdr (quail-map)))
+	(let* ((to (car map))
+	       (from (quail-get-translation
+		      (cadr map) (char-to-string to) 1)))
+	  (when (and (characterp from) (characterp to))
+	    (dolist (mod modifiers)
+	      (define-key local-function-key-map
+		(vector (append mod (list from)))
+		(vector (append mod (list to)))))))))
+    (when input-method
+      (activate-input-method current))))
+
+;; Hotkeys on russian layout
+(reverse-input-method 'russian-computer)
